5aa7edf7e0380dcdb364209864891248a7e3e66d
Chantell Conrey <chantellconrey322@gmail.com>
2025-03-09 17:58:10 -0500

    code added to prototye file

--- prototypes.js
+++ prototypes.js
@@ -1,1 +1,14 @@
+// Constructor Function for Animal
+function Animal(type, sound) {
+    this.type = type;
+    this.sound = sound;
+}
 
+// Adding a method to the prototype
+Animal.prototype.makeSound = function() {
+    console.log(`${this.type} says ${this.sound}`);
+};
+
+// Instantiate an object
+const dog = new Animal("Dog", "Woof");
+dog.makeSound(); // Output: Dog says Woof
